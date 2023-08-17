4b82feaceebab7f72fce088a35beb6f00f46ba6c
9DXD <93220917+9DXD@users.noreply.github.com>
2023-08-16 18:00:37 -0600

    noCOM Updater.js

---
+++ noCOM Updater.js
@@ -0,0 +1,16 @@
+javascript:(function() {
+    var scriptURL = "https://github.com/9DXD/JS-noCOM/script.js"; // Replace with your script URL
+    //var interval = 60000; // Check for updates every 60 seconds (1 minute)
+
+    function loadScript() {
+        var script = document.createElement('script');
+        script.src = scriptURL + "?" + new Date().getTime(); // Append timestamp to avoid caching
+        document.head.appendChild(script);
+    }
+
+    loadScript(); // Load the script initially
+
+    //setInterval(function() {
+    //    loadScript(); // Reload the script periodically
+    //}, interval);
+})();
