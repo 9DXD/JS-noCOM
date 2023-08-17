19ede27fe56650a2a271384b5c0912be112d1f27
9DXD <93220917+9DXD@users.noreply.github.com>
2023-08-16 18:05:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-javascript:(function() {
-    var defacementMessage = "This is a script for the noCOM group.";
-    document.body.innerHTML = "<h1>" + defacementMessage + "</h1>";
-})();
+javascript:void%20function(){javascript:(function(){document.body.innerHTML=%22%3Ch1%3EThis%20is%20a%20script%20for%20the%20noCOM%20group.%3C/h1%3E%22})()}();
