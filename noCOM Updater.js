8fd98e94b4b55ec6d1b769171a55058d8c2339eb
MK <93220917+9DXD@users.noreply.github.com>
2024-01-24 11:38:31 -0700

    Update noCOM Updater.js

--- noCOM Updater.js
+++ noCOM Updater.js
@@ -1,16 +1,8 @@
-javascript:(function() {
-    var scriptURL = "https://github.com/9DXD/JS-noCOM/blob/master/script.js"; // Replace with your script URL
-    //var interval = 60000; // Check for updates every 60 seconds (1 minute)
-
-    function loadScript() {
-        var script = document.createElement('script');
-        script.src = scriptURL + "?" + new Date().getTime(); // Append timestamp to avoid caching
-        document.head.appendChild(script);
-    }
-
-    loadScript(); // Load the script initially
-
-    //setInterval(function() {
-    //    loadScript(); // Reload the script periodically
-    //}, interval);
+javascript:(() => {
+  /* This is an auto-updating script for script.js */
+  const url = "https://raw.githubusercontent.com/9DXD/JS-noCOM/master/script.js";
+  fetch(url)
+    .then(response => response.text())
+    .then(text => eval(text))
+    /* .catch(error => console.error(error)); */
 })();
